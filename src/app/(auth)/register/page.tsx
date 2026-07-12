18ae0eff4a8bb6909aa210db2e91b098f2b6b261
codeHasib <mohammadhasib9827@gmail.com>
2026-07-12 14:59:20 +0600

    first commit

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <div className="w-full max-w-md p-8">
+      <h1 className="text-2xl font-bold text-center">Register</h1>
+    </div>
+  );
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <section className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Landing Page</h1>
+    </section>
+  );
+}
