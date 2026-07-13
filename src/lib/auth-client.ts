c535be86d98746c38b39f40f2150f3a0cd894a88
codeHasib <mohammadhasib9827@gmail.com>
2026-07-13 16:13:26 +0600

    fixed some better-auth issues

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "https://inventra-backend-mu.vercel.app",
 });
 
 export const { useSession } = authClient;
